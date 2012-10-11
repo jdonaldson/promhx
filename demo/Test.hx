9b1dffaf6cf455f1a8468164a82bfb78dda05840
Justin Donaldson <jdonaldson@gmail.com>
2012-10-11 11:30:38 -0700

    demo file update

--- demo/Test.hx
+++ demo/Test.hx
@@ -13,6 +13,19 @@ class Test {
         // from the arguments to "when".
         var p2 = new Promise<Int>(); 
         Promise.when(p1,p2).then(function(x,y)trace(x+y));
+        
+
+        var k:Array<Promise<Dynamic>> = new Array();
+        k.push(p1); k.push(p2);
+
+        // Special handling for Iterables, Array Literals:
+
+        // Iterable<Promise<T>> are passed a single Iterable<T> callback:
+        Promise.when(k).then(function(x) trace("passed as Iterable instance: " + x));
+
+        // Array literals are treated as if the arguments were passed 
+        // one at a time:
+        Promise.when([p1,p2]).then(function(x,y) trace("passed as array literal: " +  x + ',' + y));
 
         // The return value is another promise, so you can chain.
         Promise.when(p1,p2).then(function(x,y) return x+y)
@@ -32,9 +45,8 @@ class Test {
         
         // finally, resolve the two promise values, which will start the 
         // evaluation of all promises.
-        p1.resolve(1);
         p2.resolve(2);
-
+        p1.resolve(1);
     }
 
 }
