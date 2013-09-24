a18c778840e1db593eac7c28101bf6e7e1df4e83
Justin Donaldson <jdonaldson@gmail.com>
2013-09-24 00:05:46 -0700

    add a few simple tests

--- src/test/TestAll.hx
+++ src/test/TestAll.hx
@@ -1,5 +1,5 @@
-import sys.db.TestPostgres;
 import haxe.unit.TestRunner;
+import promhx.TestPromhx;
 class TestAll {
 	static function main() {
 		var r = new TestRunner();
--- src/test/promhx/TestPromhx.hx
+++ src/test/promhx/TestPromhx.hx
@@ -4,9 +4,23 @@ import haxe.unit.TestCase;
 import promhx.Promise;
 
 class TestPromhx extends TestCase {
-    public function testThen(){
+
+    public function testSimpleThen(){
         var p1 = new Promise<Int>();
-        p1.then(function(x) trace("delivered " + x));
-        p1.resolve(1);
+        var expected = 1;
+        p1.then(function(x) assertEquals(x,expected));
+        p1.resolve(expected);
+    }
+
+    public function testSimpleWhen(){
+        var p1 = new Promise<Int>();
+        var p2 = new Promise<Int>();
+        var expected1 = 4;
+        var expected2 = 5;
+        Promise.when(p1,p2).then(function(x,y){
+            assertEquals(x + y, expected1 + expected2);
+        });
+        p1.resolve(expected1);
+        p2.resolve(expected2);
     }
 }
