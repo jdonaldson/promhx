a06be254e3df2b96d325d7cbb28d83e76354b25d
Justin Donaldson <jdonaldson@gmail.com>
2013-10-05 19:48:28 -0700

    update promisem test

--- src/test/promhx/TestPromiseM.hx
+++ src/test/promhx/TestPromiseM.hx
@@ -5,20 +5,23 @@ import promhx.PromiseM;
 
 class TestPromiseM extends TestCase {
     public function testSimpleDo(){
-        var p2 = new Promise<String>();
         var p1 = new Promise<Int>();
+        var p2 = new Promise<String>();
 
         var p3 = PromiseM.dO({
             val1 <= p1;
             val2 <= p2;
+            val3 <= p2;
             ret({val1:val1, val2:val2});
-        }).then(function(y) trace(y.val2));
-
+        });
+        p3.then(function(y) {
+            assertEquals(y.val1, 10);
+            assertEquals(y.val2, 'foo');
+        });
 
         p1.resolve(10);
         p2.resolve('foo');
 
-        assertEquals(1,1);
     }
 
 
