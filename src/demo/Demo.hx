fa7cf807fee015f63e78352f2a49b1e9085bd7e9
Justin Donaldson <jdonaldson@gmail.com>
2013-11-30 17:52:36 -0600

    add demo related files and classes

--- src/demo/Demo.hx
+++ src/demo/Demo.hx
@@ -1,25 +1,20 @@
-import js.Browser;
-import js.JQuery;
-using js.promhx.JQueryTools;
+// import js.Browser;
+// import js.JQuery;
 import promhx.Stream;
+using js.promhx.JQueryTools;
+import js.JQuery;
+using promhx.haxe.EventTools;
 
 class Demo {
     static function main() {
-        var target1 = new JQuery("#target1"),
-            target2 = new JQuery("#target2"),
-            s1 = target1.clickStream(),
-            s2 = target2.clickStream(),
-            markclicked = function(id:Int, x:JqEvent){
-                x.currentTarget.innerHTML = 'target $id clicked';
-            };
-
-        s1.then(markclicked.bind(1,_));
-        s2.then(markclicked.bind(2,_));
-        
-        var status = new JQuery("#status");
-        var s3 = Stream.whenever(s1,s2).then(function(x,y){
-            status.html('clicked target 1 on ${x.timeStamp} 
-                    and  clicked target 2 on ${y.timeStamp}');
+        var j = new JQuery("#status").eventStream('focus');
+        var h = new promhx.haxe.Http("test.txt");
+        h.then(function(x){
+            trace(x);
+        });
+        h.request();
+        j.then(function(x){
+            trace(x);
         });
     }
 }
