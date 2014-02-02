7d5d62b3686ba71cc8bfe4ac96ea0e45370102e6
Justin Donaldson <jdonaldson@gmail.com>
2014-02-01 16:46:29 -0800

    bundle setImmediate so haxelib works

--- tools/bundle.sh
+++ tools/bundle.sh
@@ -5,6 +5,8 @@ echo "removing promhx.zip"
 rm promhx.zip
 
 cp haxelib.json haxelib
+mkdir -p haxelib/lib/setImmediate
+cp ./lib/setImmediate/setImmediate.js haxelib/lib/setImmediate/setImmediate.js
 cp -r src/main/promhx haxelib
 cp -r ./src/main/js haxelib
 
