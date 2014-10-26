033eb7ec41a52e9d6f59e713f7e4b15f3e4b8296
Justin Donaldson <jdonaldson@gmail.com>
2014-10-26 10:18:06 -0700

    publicstream gets throwError

--- src/main/promhx/PublicStream.hx
+++ src/main/promhx/PublicStream.hx
@@ -6,12 +6,9 @@ class PublicStream<T> extends Stream<T> {
     public function new(?def : Deferred<T>){
         super(def);
     }
-    inline public function resolve(val:T){
-        this.handleResolve(val);
-    }
-    inline public function update(val:T){
-        this.handleResolve(val);
-    }
+    inline public function resolve(val:T) this.handleResolve(val);
+    inline public function throwError(e:Dynamic) handleError(e);
+    inline public function update(val:T) this.handleResolve(val);
     inline public static function publicstream<T>(val:T){
         var ps = new PublicStream();
         ps.resolve(val);
