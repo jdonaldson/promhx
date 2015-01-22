d4e0474fe9daeec38ec264784a4dc961df0386d0
Ignatiev Mikhail <ignatiev.reg@gmail.com>
2015-01-22 12:30:17 +0300

    Thenable interface to reflect AsyncBase

--- src/main/promhx/Thenable.hx
+++ src/main/promhx/Thenable.hx
@@ -2,5 +2,5 @@ package promhx;
 typedef Thenable<T> = {
     function then<A>(f : T->A): Thenable<A>;
     function errorThen<A>(f : Dynamic->A): Thenable<A>;
-    function catchError<A>(f : T->T): Thenable<T>;
+    function catchError<A>(f : Dynamic-Void): Thenable<T>;	
 }
